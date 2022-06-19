ce150649bf23872ae9faf66fad5a81a39b6b7494
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-19 18:59:01 +0800

    Update registerSW.js

--- src/registerSW.js
+++ src/registerSW.js
@@ -1,8 +1,9 @@
 	window.addEventListener('load', () => {registerSW();});
-	// Register the Service Worker
 	async function registerSW() {
 	if ('serviceWorker' in navigator) {
 		try {
+		console.log('serviceWorker is in NAVIGATOR');
 		await navigator.serviceWorker.register('/web/sw.js', {scope: '/web/'});
+		console.log('registration of /web/sw.js is a success');}
 		catch (e) {console.log(e);}
 	}}
