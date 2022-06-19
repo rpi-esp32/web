1c62b62dc475a6fcb40b9a87c913b44fdd7f36c3
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-19 17:11:02 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from js import add
 from pyodide import create_proxy
 
 #def callback(e):
-    pyscript.write('target', 'this is my target')
+pyscript.write('target', 'this is my target')
 
 #add(create_proxy(callback))
 
