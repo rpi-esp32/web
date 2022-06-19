12cc08181d89f48a5835624adca024d64bf41d6e
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-19 18:25:34 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const assets = ["/web/images/fav.ico", "/web/", "/web/index.html", "https://pyscript.net/alpha/pyscript.css", "https://pyscript.net/alpha/pyscript.js", "/web/src/main.py", "/web/src/main.js"];
+const assets = ["web/manifest.json", "/web/images/fav.ico", "/web/", "/web/index.html", "https://pyscript.net/alpha/pyscript.css", "https://pyscript.net/alpha/pyscript.js", "/web/src/main.py", "/web/src/main.js"];
 
 console.log("maurice file sw.js FULL ASSETS"); 
 
