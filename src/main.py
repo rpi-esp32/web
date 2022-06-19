ad29feceb5b68679ddd3f1097afd56e12bf75c9f
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-19 17:06:36 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,10 @@
 from js import add
 from pyodide import create_proxy
-# import numpy as np
 
-#span = Element("m")
-
-def callback(e):
+#def callback(e):
     pyscript.write('target', 'this is my target')
 
-#def orientation(a):
-#    return f"hello {typeof(a)},  = {a}"
+#add(create_proxy(callback))
 
-add(create_proxy(callback))
-#
 
         
