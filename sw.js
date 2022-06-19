568f628610b09ec9a38db9d48eff192924517422
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-19 19:07:57 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 
-const assets = ["web/src/registerSW.js", "web/manifest.json", "/web/images/fav.ico", "/web/", "/web/index.html", "https://pyscript.net/alpha/pyscript.css", "https://pyscript.net/alpha/pyscript.js", "/web/src/main.py", "/web/src/main.js"];
+#const assets = ["web/src/registerSW.js", "web/manifest.json", "/web/images/fav.ico", "/web/", "/web/index.html", "https://pyscript.net/alpha/pyscript.css", "https://pyscript.net/alpha/pyscript.js", "/web/src/main.py", "/web/src/main.js"];
+const assets = ["web/src/registerSW.js", "/web/images/fav.ico", "/web/", "/web/index.html", "https://pyscript.net/alpha/pyscript.css", "https://pyscript.net/alpha/pyscript.js", "/web/src/main.py", "/web/src/main.js"];
 
 console.log("maurice file sw.js FULL ASSETS"); 
 
