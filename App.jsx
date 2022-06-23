01d09c12d8fadcb0f7cc40d5fcd1cff90fc252b4
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-23 12:59:58 +0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Test}  from './Test';
 
-class App() extends Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
