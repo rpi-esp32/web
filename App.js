91c737bd44f7b80c2e85871e634db2344d516bcb
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-23 12:34:45 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, {Component} from 'react'
+import Test  // from './components/'
 
-
-
-
-
+function App() {
+  render() {
+    return (
+      <div className="App">
+        <Test></Test>
+      </div>
+    )  
+  }  
+  
+}
 
 export default App:
