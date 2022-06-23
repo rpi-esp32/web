ef646dc8619af40b4be74de1ec66da258769a323
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-23 12:45:27 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
-import Test  // from './components/'
+import Test  from './Test'
 
 function App() {
   render() {
     return (
       <div className="App">
+        <div> from App() </div>
+        <div> from Test </div>
         <Test></Test>
       </div>
     )  
